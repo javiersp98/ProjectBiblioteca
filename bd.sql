8c15d402ceb5f98509c9bb4547c3baf4e2647ef1
javiersp98 <38034215+javiersp98@users.noreply.github.com>
2018-11-29 09:38:24 +0100

    Script de BD actualizado

--- bd.sql
+++ bd.sql
@@ -9,6 +9,8 @@ CREATE TABLE users (
     contrasena VARCHAR(20)
 );
 INSERT INTO users VALUES("admin", "admin");
+INSERT INTO users VALUES("javier", "javier");
+INSERT INTO users VALUES("juancarlos", "juancarlos");
 
 DROP TABLE IF EXISTS libro;
 CREATE TABLE libro (
@@ -18,7 +20,7 @@ CREATE TABLE libro (
     autor VARCHAR(50),
     editorial VARCHAR(50),
     tema VARCHAR(50),
-    ano INT    
+    ano INT
 );
 INSERT INTO libro VALUES(1, "isbn1", "La Caida de los Gigantes", "Ken Follet", "Editorial1", "Historia", 2010);
 INSERT INTO libro VALUES(2, "isbn2", "Una Breve Historia de Casi Todo", "Bill Bryson", "Editorial2", "Ciencia", 2004);
@@ -32,9 +34,9 @@ CREATE TABLE vinilo (
     autor VARCHAR(50),
     discografica VARCHAR(50),
     estilo VARCHAR(50),
-    ano INT 
+    ano INT
 );
-INSERT INTO vinilo VALUES(1, "The Rise and Fall of Ziggy Stardust and the Spiders from Mars", "David Bowie", "Virgin", "Glam Rock", 1972);
+INSERT INTO vinilo VALUES(1, "The Rise and Fall of Ziggy Stardust", "David Bowie", "Virgin", "Glam Rock", 1972);
 INSERT INTO vinilo VALUES(2, "Piano Man", "Billy Joel", "Columbia", "Piano Rock", 1973);
 INSERT INTO vinilo VALUES(3, "Made in Japan", "Deep Purple", "Purple Records", "Hard Rock", 1972);
 INSERT INTO vinilo VALUES(4, "Distant Light", "The Hollies", "Parlophone", "Pop Rock", 1971);
